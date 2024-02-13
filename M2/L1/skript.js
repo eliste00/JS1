1e6cd21a7c0f16fea0c416af9552c1e8b2da99f4
Elizabeta Stevic <eliste00224@stud.noroff.no>
2024-02-13 20:21:32 +0100

    answer questions 1-4

--- M2/L1/skript.js
+++ M2/L1/skript.js
@@ -1,1 +1,24 @@
-console.log("hallo");
+// question 1
+var winningNumbers = [1, 76, 2, 78, 16, 7];
+console.log(winningNumbers[2]);
+
+//Question 2
+var vegetables = ["rice", "potatoes", "porridge", "cake", "spinach"];
+vegetables.push("orange");
+console.log(vegetables);
+//Question 3
+var randomThings = ["pumpkin", false, 23, true, "tomato"];
+for (var i = 0; i < randomThings.length; i++) {
+  var element = randomThings[i];
+  console.log(element);
+}
+//Question 4
+var moreThings = [14, "cat", true, 29, "fox", true, 34, 19];
+for (let index = 0; index < moreThings.length; index++) {
+  const element = moreThings[index];
+  if (typeof element === "number") {
+    console.log(element);
+  }
+}
+//Question 5
+var ingredients = ["water", "flour", "toothpaste", "fish lips"];
