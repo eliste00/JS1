e3044da161aad52375983b4030fb1542d2aa7eea
Elizabeta Stevic <eliste00224@stud.noroff.no>
2024-02-17 14:17:04 +0100

    Question4

--- M2/L2/script.js
+++ M2/L2/script.js
@@ -36,15 +36,39 @@ var dinnerOptions = [
 ];
 
 for (var i = 0; i < dinnerOptions.length; i++) {
-    const element = dinnerOptions[i];
-    if (dinnerOptions ==  ) {
-     console.log();   
-    }
-    }
-console.log(result);
+  const dinner = dinnerOptions[i];
+  if (dinner.onSpecial === true) {
+    console.log(dinner.name);
+  }
+}
 
-
-
-
-console,log(Bacon and Frogs);
-console.log(Mince Flies);
+//Question 4
+var people = [
+  {
+    firstName: "Burt",
+    lastName: "Smacharach",
+    age: 35,
+  },
+  {
+    firstName: "Elvis",
+    lastName: "Deadly",
+    age: 55,
+  },
+  {
+    firstName: "Susan",
+    lastName: "Sarongson",
+    age: 42,
+  },
+  {
+    firstName: "Big",
+    lastName: "Gabe",
+    age: 20,
+  },
+];
+for (let index = 0; index < people.length; index++) {
+  const person = people[index];
+  if (person.age >= 20 && person.age <= 50) {
+    console.log(`${person.firstName} ${person.lastName}`);
+  }
+}
+//Question 5
